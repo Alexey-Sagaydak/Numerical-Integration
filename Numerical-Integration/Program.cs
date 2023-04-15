726e61da69a95933660a5946c62c5e20377722f6
Alexey-Sagaydak <99820328+Alexey-Sagaydak@users.noreply.github.com>
2023-04-15 17:41:06 +0700

    симППППППсон

--- Numerical-Integration/Program.cs
+++ Numerical-Integration/Program.cs
@@ -13,11 +13,10 @@ public static class Program
     private static void Run()
     {
         float Function(float x) => (float)((1 + x) / (Math.Pow(2 + 3 * x, 2) * Math.Sqrt(2 + x)));
-        int a = 5, b = 15;
-        float e1 = 0.001f, e2 = 0.0001f;
         
         MathFunction mathFunction = new MathFunction(Function);
         Menu menu = new Menu(mathFunction);
+
         menu.Run();
     }
 }
